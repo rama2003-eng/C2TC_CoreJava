ef18b1e2fd851e6518c89aa9cbec39ee978084f0
Asus <Asus@LAPTOP-ORVU1I7P>
2023-07-27 11:23:00 +0530

    FIRST

---
+++ C2TC/src/com/cg/dayone/Basics.java
@@ -0,0 +1,20 @@
+package com.cg.dayone;
+
+import java.util.Scanner;
+
+public class Basics {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		System.out.println("Sum :"+(a+b));
+		System.out.println("Subraction :"+(a-b));
+		System.out.println("Multiplication:"+(a*b));
+		System.out.println("Divide :"+(a/b));
+		System.out.println("Modulus :"+(a%b));
+
+	}
+
+}
